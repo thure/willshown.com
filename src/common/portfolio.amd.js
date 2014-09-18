fe1600a067a96cba9441193b4d45c1eb338e3a18
Will Shown <w@willshown.com>
2014-09-17 23:57:51 -0700

    One-up and all-up views are in-place and ready to be developed.

--- src/common/portfolio.amd.js
+++ src/common/portfolio.amd.js
@@ -7,10 +7,21 @@ define([
 ){
 
   return new function(){
-    var self = this;
+    var self = this
+      , viels = {
+      $page: $('section[data-page="portfolio"]'),
+//      $splash: $('section[data-page="portfolio"] .splash'),
+      $portfolio: $('section[data-page="portfolio"] .portfolio'),
+//      $allUp: $('section[data-page="portfolio"] .portfolio section.portfolio_all-up'),
+//      $oneUp: $('section[data-page="portfolio"] .portfolio section.portfolio_one-up')
+    };
 
     this.open = function(){
-      console.log('Portfolio open!');
+      viels.$page.attr('data-displaying', 'portfolio');
+    };
+
+    this.portfolioView = function(view){
+      viels.$portfolio.attr('data-displaying', view);
     }
 
   };
