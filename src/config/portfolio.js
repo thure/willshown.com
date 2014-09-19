77e1b8dafa260354b35479924deef2e566116388
Will Shown <w@willshown.com>
2014-09-18 22:40:47 -0700

    Adding portfolio items.

---
+++ src/config/portfolio.js
@@ -0,0 +1,27 @@
+define({
+
+  'MSN Prime': {
+    'Diagonal motion carousel': {},
+    'Editable design prototype': {},
+    'Flexible content display': {},
+    'Left nav prototype': {}
+  },
+  'Apigee Enterprise': {
+    'Thunderpony': {},
+    'Redesign': {},
+    'Continuing use': {}
+  },
+  'Works in progress': {
+    'Pi Brewer': {},
+    'Baxter': {},
+    'GEOF': {}
+  },
+  'Other work': {
+    'Cardboard.coop': {},
+    'TownHog': {},
+    'CINCH': {
+      // http://mheonline.com/cinch/
+    }
+  }
+
+});
