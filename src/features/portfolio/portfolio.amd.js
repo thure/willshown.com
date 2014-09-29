5f4946b1c9596b9b8a096283bd12960f6b42f5c4
Will Shown <w@willshown.com>
2014-09-29 10:15:16 -0700

    Added all the hooks for opening the one-up view.

--- src/features/portfolio/portfolio.amd.js
+++ src/features/portfolio/portfolio.amd.js
@@ -3,10 +3,11 @@ define([
   'underscore',
   'q',
   'config/portfolio',
+  'amd/sci',
   'text!amd/all-up.ejs',
   'text!amd/one-ups.ejs'
 ], function(
-  $, _, q, portfolio, allUpEJS, oneUpsEJS
+  $, _, q, portfolio, sci, allUpEJS, oneUpsEJS
 ){
 
   return new function(){
@@ -21,27 +22,51 @@ define([
       , oneUpsT = _.template(oneUpsEJS);
 
     this.render = function(){
-      viels.$allUp.html(allUpT({portfolio: portfolio}));
-      viels.$portfolio.append(oneUpsT({portfolio: portfolio}));
+      var $allUp = $(allUpT({portfolio: portfolio}))
+        , $oneUps = $(oneUpsT({portfolio: portfolio}));
+      viels.$allUp.html($allUp);
+      viels.$portfolio.append($oneUps);
+      self.bind({
+        $allUp: $allUp,
+        $oneUps: $oneUps
+      });
+    };
+
+    this.bind = function(els){
+      sci.then(function(i) {
+        els.$allUp.on('click touchstart', 'a.portfolio-item', function ($e) {
+          var name = $e.target.hasAttribute('data-name') ?
+            $e.target.getAttribute('data-name') :
+            $($e.target).parents('[data-name]').attr('data-name');
+          i.gen({
+            name: 'down',
+            data: name
+          });
+        });
+      });
     };
 
     this.open = function(){
       viels.$page.attr('data-displaying', 'portfolio');
     };
 
-    this.portfolioView = function(view){
+    this.portfolioView = function(view, section){
       viels.$portfolio.attr('data-displaying', view);
       switch(view){
         case 'all-up':
           viels.$main.addClass('start-all-up');
           break;
+        case 'one-up':
+          viels.$main.addClass('start-one-up');
+          console.log('Opening ' + section);
       }
     };
 
-    this.exit = function(view){
+    this.exit = function(view, exitingTo){
       switch(view){
         case 'all-up':
           viels.$main.removeClass('start-all-up');
+          console.log('Exiting to ' + exitingTo);
           break;
       }
     };
