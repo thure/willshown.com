04a88f6b386112efc55f57794ba99b2864f77f91
Will Shown <w@willshown.com>
2014-10-03 19:23:15 -0700

    Added copy for all the different oeuvres.

--- src/config/portfolio.js
+++ src/config/portfolio.js
@@ -1,116 +1,150 @@
-define({
+define([
+  'underscore',
+  'text!amd/baxter.ejs',
+  'text!amd/brewer.ejs',
+  'text!amd/cardboard.ejs',
+  'text!amd/cinch.ejs',
+  'text!amd/continuing.ejs',
+  'text!amd/diagonal.ejs',
+  'text!amd/flex.ejs',
+  'text!amd/geof.ejs',
+  'text!amd/icex.ejs',
+  'text!amd/leftnav.ejs',
+  'text!amd/redesign.ejs',
+  'text!amd/thunderpony.ejs',
+  'text!amd/townhog.ejs'
+], function(
+  _,
+  baxter,
+  brewer,
+  cardboard,
+  cinch,
+  continuing,
+  diagonal,
+  flex,
+  geof,
+  icex,
+  leftnav,
+  redesign,
+  thunderpony,
+  townhog
+  ){
 
-  'MSN Prime': {
-    'Diagonal motion carousel': {
-      asset: {
-        type: 'video',
-        name: 'diagonal'
+  return{
+
+    'MSN Prime': {
+      'Diagonal motion carousel': {
+        asset: {
+          type: 'video',
+          name: 'diagonal'
+        },
+        role: 'prototyper',
+        description: diagonal
       },
-      role: 'prototyper',
-      description: 'Doop'
+      'Editable design prototype': {
+        asset: {
+          type: 'video',
+          name: 'icex'
+        },
+        role: 'prototyper',
+        description: icex
+      },
+      'Flexible content display': {
+        asset: {
+          type: 'video',
+          name: 'flex'
+        },
+        role: ['designer', 'developer'],
+        description: flex
+      },
+      'Left nav prototype': {
+        asset: {
+          type: 'video',
+          name: 'leftnav'
+        },
+        role: ['interaction designer', 'prototyper'],
+        description: leftnav
+      }
     },
-    'Editable design prototype': {
-      asset: {
-        type: 'video',
-        name: 'icex'
+    'Apigee Enterprise': {
+      'Thunderpony': {
+        asset: {
+          type: 'image',
+          name: 'thunderpony.png'
+        },
+        role: 'prototyper',
+        description: thunderpony
       },
-      role: 'prototyper',
-      description: 'Doop'
+      'Redesign': {
+        asset: {
+          type: 'image',
+          name: 'redesign.png'
+        },
+        role: 'designer',
+        description: redesign
+      },
+      'Continuing use': {
+        asset: {
+          type: 'image',
+          name: 'continuing.png'
+        },
+        role: 'none',
+        description: continuing
+      }
     },
-    'Flexible content display': {
-      asset: {
-        type: 'video',
-        name: 'flex'
+    'Works in progress': {
+      'Pi Brewer': {
+        asset: {
+          type: 'image',
+          name: 'brewer.jpeg'
+        },
+        role: 'hacker',
+        description: brewer
       },
-      role: ['designer', 'developer'],
-      description: 'Doop'
+      'Doorbot': {
+        asset: {
+          type: 'image',
+          name: 'baxter.jpeg'
+        },
+        role: 'hacker',
+        description: baxter
+      },
+      'GEOF': {
+        asset: {
+          type: 'image',
+          name: 'geof.jpeg'
+        },
+        role: ['designer', 'developer'],
+        description: geof
+      }
     },
-    'Left nav prototype': {
-      asset: {
-        type: 'video',
-        name: 'leftnav'
+    'Other work': {
+      'Cardboard.coop': {
+        asset: {
+          type: 'video',
+          name: 'cardboard'
+        },
+        role: ['designer', 'developer'],
+        description: cardboard
       },
-      role: ['interaction designer', 'prototyper'],
-      description: 'Doop'
+      'TownHog': {
+        asset: {
+          type: 'image',
+          name: 'townhog.png'
+        },
+        role: 'developer',
+        description: townhog
+      },
+      'CINCH': {
+        asset: {
+          type: 'video',
+          name: 'cinch'
+        },
+        role: 'developer',
+        description: cinch
+      }
     }
-  },
-  'Apigee Enterprise': {
-    'Thunderpony': {
-      asset: {
-        type: 'image',
-        name: 'thunderpony.png'
-      },
-      role: 'prototyper',
-      description: 'Doop'
-    },
-    'Redesign': {
-      asset: {
-        type: 'image',
-        name: 'redesign.png'
-      },
-      role: 'designer',
-      description: 'Doop'
-    },
-    'Continuing use': {
-      asset: {
-        type: 'image',
-        name: 'continuing.png'
-      },
-      role: 'none',
-      description: 'Doop'
-    }
-  },
-  'Works in progress': {
-    'Pi Brewer': {
-      asset: {
-        type: 'image',
-        name: 'brewer.jpeg'
-      },
-      role: 'hacker',
-      description: 'Doop'
-    },
-    'Baxter': {
-      asset: {
-        type: 'image',
-        name: 'baxter.jpeg'
-      },
-      role: 'hacker',
-      description: 'Doop'
-    },
-    'GEOF': {
-      asset: {
-        type: 'image',
-        name: 'geof.jpeg'
-      },
-      role: ['designer', 'developer'],
-      description: 'Doop'
-    }
-  },
-  'Other work': {
-    'Cardboard.coop': {
-      asset: {
-        type: 'video',
-        name: 'cardboard'
-      },
-      role: ['designer', 'developer'],
-      description: 'Doop'
-    },
-    'TownHog': {
-      asset: {
-        type: 'image',
-        name: 'townhog.png'
-      },
-      role: 'developer',
-      description: 'Doop'
-    },
-    'CINCH': {
-      asset: {
-        type: 'video',
-        name: 'cinch'
-      },
-      role: 'developer',
-      description: 'Doop'
-    }
+
   }
 
 });
